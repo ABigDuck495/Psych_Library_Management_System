69962bd1d19794d192dcf664efb8d39cfd814915
Ruidono <louierey.mercado@clsu2.edu.ph>
2025-10-13 21:47:29 +0800

    Authors

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
 use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-    //
+    public function index()
+    {
+        $authors = Author::orderBy('last_name')->paginate(10);
+        return view('authors.index', compact('authors'));
+    }
+
+    public function create()
+    {
+        return view('authors.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+        ]);
+
+        Author::create($request->only('first_name', 'last_name'));
+
+        return redirect()->route('authors.index')->with('success', 'Author added successfully!');
+    }
+
+    public function edit(Author $author)
+    {
+        return view('authors.edit', compact('author'));
+    }
+
+    public function update(Request $request, Author $author)
+    {
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+        ]);
+
+        $author->update($request->only('first_name', 'last_name'));
+
+        return redirect()->route('authors.index')->with('success', 'Author updated successfully!');
+    }
+
+    public function destroy(Author $author)
+    {
+        $author->delete();
+        return redirect()->route('authors.index')->with('success', 'Author deleted successfully!');
+    }
 }
