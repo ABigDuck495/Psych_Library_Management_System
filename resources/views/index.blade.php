70ce182d71457c795c59ca69b3d0822fc549ff73
Ruidono <louierey.mercado@clsu2.edu.ph>
2025-10-13 21:49:09 +0800

    basic crud lang yan ng authors

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome to Psych Library Management System</h1>
+
+    <!-- Button linking to authors index -->
+    <a href="{{ route('authors.index') }}">
+        <button type="button">Go to Authors</button>
+    </a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::resource('authors', AuthorController::class);
