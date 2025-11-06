56a6e82703d411fe8b55e89c6dfcf418a8e85ced
ABigDuck495 <abigduck495@gmail.com>
2025-11-06 18:34:46 +0800

    final i think

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ php artisan migrate --force
 php artisan db:seed --force
 
 # Start PHP-FPM in background
-php-fpm -y app/php-fpm.conf &
+php-fpm -y /php-fpm.conf &
 
 # Start Nginx with our custom config
 nginx -g 'daemon off;'
