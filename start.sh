4326b1d17f46caa863e00c3778594b7a9960520e
ABigDuck495 <abigduck495@gmail.com>
2025-11-06 17:22:29 +0800

    bumalik ka nginx

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "Running database migrations..."
 php artisan migrate --force
-
-echo "Running database seeds..."
 php artisan db:seed --force
 
+# Start PHP-FPM in background
+php-fpm &
+
+# Start Nginx in foreground
+nginx -g 'daemon off;'
