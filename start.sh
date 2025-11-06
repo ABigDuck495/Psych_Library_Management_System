48ba3781626ca9d1d44f5afc67416ebc8b5b65b6
ABigDuck495 <abigduck495@gmail.com>
2025-11-06 16:57:49 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -7,5 +7,8 @@ php artisan migrate --force
 echo "Running database seeds..."
 php artisan db:seed --force
 
-echo "Starting application..."
-exec /usr/bin/supervisord -c /etc/supervisord.conf
+echo "Starting PHP-FPM..."
+php-fpm83 -D
+
+echo "Starting Nginx..."
+exec nginx -g 'daemon off;'
