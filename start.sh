0769ffffddab83693de9c41a294b06e13073f183
ABigDuck495 <abigduck495@gmail.com>
2025-11-06 17:30:18 +0800

    engine x fix

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 set -e
 
+# Copy nginx config to the correct location
+cp nginx.conf /etc/nginx/nginx.conf
+
 php artisan migrate --force
 php artisan db:seed --force
 
 # Start PHP-FPM in background
 php-fpm &
 
-# Start Nginx in foreground
+# Start Nginx with our custom config
 nginx -g 'daemon off;'
